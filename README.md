ca5415a98e78974fe6dceb0369f9c03428dc6d5e
Phil <pw@optimanet.ch>
2020-09-22 16:54:46 +0200

    first release

---
+++ README.md
@@ -0,0 +1,10 @@
+# Citrix VNC console forwarding
+To forward the VNC-Service that are provided by the Citrix Hypervisor, you can use a simple node.js script. The procedure is documentated under https://developer-docs.citrix.com/projects/citrix-hypervisor-sdk/en/latest/xs-api-extensions/
+
+The node.js script must be hosted on a guest-system on your cluster and they must have access to the master-server on port 443 (which provides the VNC service). The scripts starts a tcp-server, that your VNC-client can connect from outside, and forward the connection to the master.
+
+The only tricky step is the initializing. First you need to send the initial http-header to authorize you, and on the response you must remove the http-header, because VNC-clients can't handle this.
+
+## References
+[Procedure](https://developer-docs.citrix.com/projects/citrix-hypervisor-sdk/en/latest/xs-api-extensions/)
+[API documentation]([Documentation](https://developer-docs.citrix.com/projects/citrix-hypervisor-sdk/en/latest/xs-api-extensions/))
---
+++ vnc-example.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const net = require('net');
+const tls = require('tls');
+
+const server = net.createServer((serverSocket) => {
+    const options = {
+        host: '123.123.123.123',
+        port: 443,
+        rejectUnauthorized: false
+    };
+
+    const client = tls.connect(options, () => {
+        // Set http parameters
+        client.write(
+            [
+                'CONNECT /console?uuid=1234b76b-1234-1234-1234-12341e556adb HTTP/1.1',
+                'Host: 123.123.123.123',
+                'Cookie: session_id=OpaqueRef:1234b58c-1234-1234-1234-1234c89aed86',
+                '',
+                '',
+            ].join('\r\n')
+        )
+    });
+
+    let passthrough = false;
+    let cache = '';
+
+    client.on('data', (data) => {
+        if (passthrough)
+            serverSocket.write(data);
+        else {
+            cache += data.toString();
+
+            if (cache.includes('\r\n\r\n')) {
+                passthrough = true;
+                serverSocket.write(cache.split('\r\n\r\n')[1]);
+            }
+        }
+    });
+
+    client.on('end', () => {
+    })
+
+    client.on('error', (error) => {
+    })
+
+    serverSocket.on('data', (data) => {
+        client.write(data);
+    });
+
+    serverSocket.on('end', () => {
+    });
+
+    serverSocket.on('error', (error) => {
+    })
+});
+
+server.listen(1234, '127.0.0.1');
